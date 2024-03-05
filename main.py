b1ed66c174805aa22b711252c20bb3084b2fd64f
Gpbhhxx <2714446205@qq.com>
2024-03-05 17:19:47 +0800

    字符串功能介绍

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+这是介绍字符串功能函数
+2024-3-5
+"""
+a = "HelloWord"
+print(a[2])  # 字符串索引
+print(a[1:3])  # 字符串切片，左包含右不包含
+print(a.title())  # 各个单词首字母大写
+print(a.upper())  # 所有字母大写
+print(a.lower())  # 所有字母小写
